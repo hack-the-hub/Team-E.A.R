36d9f451ea217184efdbfadb59bcccf37f81cce5
Elliot Brown <imodz03@gmail.com>
2016-09-10 12:18:22 +0100

    Added more fields to facilities

--- Actissible-Service/src/main/java/Domain/Facilities.java
+++ Actissible-Service/src/main/java/Domain/Facilities.java
@@ -1,17 +1,23 @@
 package Domain;
 
+import lombok.Data;
+
 /**
  * Created by elliotb on 10/09/2016.
  */
+@Data
 public class Facilities {
 
+    private double lat;
+    private double lon;
     private String name;
     private String addressLine1;
     private String town;
     private String county;
     private String postcode;
     private String districtCounty;
-
-
+    private String telephoneNumber;
+    private String ownershipType;
+    private boolean adventure_sport;
 
 }
